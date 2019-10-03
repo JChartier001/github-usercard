cbbacbe01e9e095d19df7b3e14d826ed549b23fe
JChartier001 <JChartier001@gmail.com>
2019-10-03 18:18:14 -0500

    Finished MVP and stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,28 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/JChartier001')      
+    .then(response => {
+        console.log(response);
+        createCard(response.data);
+        })
+    .catch(error => {
+        console.log(error)
+    });
+
+const followersArray = [];
+
+axios.get('https://api.github.com/users/JChartier001/followers')
+    .then(response => {
+        //going through response "array"
+        response.data.forEach(async (githubUser) => {
+            //setting userResponse to get usernames.  "await" sets interval.
+            const userResponse = await axios.get(githubUser.url);
+            console.log(userResponse)
+           createCard(userResponse.data);
+        })
+        
+    })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -23,8 +45,6 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
-const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +73,51 @@ const followersArray = [];
   luishrd
   bigknell
 */
+const entryPoint = document.querySelector('.cards');
+function createCard(data) {
+
+    //create elements
+    const container = document.createElement('div');
+    const img = document.createElement('img');
+    const cardInfo = document.createElement('div');
+    const name = document.createElement('h3');
+    const userName = document.createElement("p");
+    const location = document.createElement('p');
+    const profile = document.createElement('p');
+    const profileLink = document.createElement('a');
+    const followers = document.createElement('p');
+    const following = document.createElement('p');
+    const bio = document.createElement('p');
+
+    //adding class
+    container.classList.add("card");
+    cardInfo.classList.add("card-info");
+    name.classList.add('name');
+    userName.classList.add('username');
+
+    //adding text
+    img.src = data.avatar_url;
+    name.textContent = data.name;
+    userName.textContent = data.login;
+    location.textContent = "Location:  " + data.location;
+    profile.textContent = "Profile: ";
+    profileLink.textContent = data.html_url;
+    followers.textContent = "Followers " + data.followers;
+    following.textContent = "Following " + data.following;
+    bio.textContent = "Bio " + data.bio;
+
+    //appending
+    container.appendChild(img);
+    container.appendChild(cardInfo);
+    cardInfo.appendChild(name);
+    cardInfo.appendChild(location);
+    
+    cardInfo.appendChild(profile);
+    profile.appendChild(profileLink);
+    cardInfo.appendChild(followers);
+    cardInfo.appendChild(following);
+    cardInfo.appendChild(bio);
+    
+
+    entryPoint.appendChild(container);
+}
